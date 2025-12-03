a3f28bda9196c5fa2709d8671bead26bfb9c6760
VNReality <viveknprofessional@gmail.com>
2025-12-03 15:34:35 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,11 @@
+V-Tube PWA (GitHub Pages ready)
+
+Files:
+- index.html
+- manifest.json
+- sw.js
+- icons/icon-192.png
+- icons/icon-512.png
+
+Put these at the ROOT of your GitHub repo (or the root of the branch you publish as GitHub Pages).
+All paths are relative, so it will work whether the site is at / or /repo-name/.
---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'vtube-cache-v1';
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) return cachedResponse;
+        return fetch(event.request).then(networkResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        }).catch(() => {
+          if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+        });
+      })
+    );
+  }
+});
